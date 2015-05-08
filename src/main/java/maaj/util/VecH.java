64b5e40c9e3b246b1e6c0a19d13792eb5fba0e59
Maartyl <kuronomaartyl@gmail.com>
2015-05-08 18:14:28 +0200

    cleaned

--- src/main/java/maaj/util/VecH.java
+++ src/main/java/maaj/util/VecH.java
@@ -5,16 +5,10 @@
  */
 package maaj.util;
 
-import com.github.krukow.clj_lang.IEditableCollection;
-import com.github.krukow.clj_lang.IPersistentVector;
-import com.github.krukow.clj_lang.ITransientVector;
-import com.github.krukow.clj_lang.PersistentVector;
 import maaj.coll.Tuple0;
 import maaj.coll.traits.VecLike;
-import maaj.coll.wrap.VecTWrap;
 import maaj.term.Invocable2;
 import maaj.term.Seq;
-import maaj.term.Term;
 import maaj.term.Vec;
 import maaj.term.VecT;
 
@@ -31,34 +25,19 @@ public class VecH {
     return H.indexed2Seq(v); //VecLike makes it safe : immutable
   }
 
-  public static Seq asSeq(VecT v) {
-    return asSeq(v.asPersistent());
-  }
-
   public static Vec fromSeq(Seq s) {
     return ((VecT) s.reduce(emptyTransient(), (Invocable2) (v, x) -> ((VecT) v).doConj(x))).asPersistent();
   }
 
   public static int computeHash(Vec v) {
-    //TODO: compose hash
-    return 5;
+    //TODO: compose hash for tuples: has to work same as clj vecs
+    throw new UnsupportedOperationException("not yet");
   }
 
   public static Vec emptyPersistent() {
     return Tuple0.EMPTY_VEC;
   }
   public static VecT emptyTransient() {
-    return VecTWrap.of(emptyTransientClj());
+    return emptyPersistent().asTransient();
   }
-
-  @SuppressWarnings({"unchecked"})
-  public static IPersistentVector<Term> emptyPersistentClj() {
-    return PersistentVector.EMPTY;
-  }
-
-  @SuppressWarnings("unchecked")
-  public static ITransientVector<Term> emptyTransientClj() {
-    return (ITransientVector<Term>) (((IEditableCollection) emptyPersistentClj()).asTransient());
-  }
-
 }
