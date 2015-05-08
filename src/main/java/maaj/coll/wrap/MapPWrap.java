73bb1744020ad7294dc116e53dca66d6ae92e2b9
Maartyl <kuronomaartyl@gmail.com>
2015-05-08 18:44:01 +0200

    Map : ~mostly; without serialization

--- src/main/java/maaj/coll/wrap/MapPWrap.java
+++ src/main/java/maaj/coll/wrap/MapPWrap.java
@@ -5,14 +5,21 @@
  */
 package maaj.coll.wrap;
 
+import com.github.krukow.clj_lang.IPersistentMap;
 import java.util.Iterator;
+import java.util.Map.Entry;
+import maaj.coll.traits.Functor;
 import maaj.term.Int;
 import maaj.term.Invocable;
+import maaj.term.Invocable1;
 import maaj.term.KVPair;
 import maaj.term.Map;
 import maaj.term.MapT;
 import maaj.term.Seq;
 import maaj.term.Term;
+import maaj.util.H;
+import maaj.util.MapH;
+import maaj.util.SeqH;
 
 /**
  *
@@ -20,14 +27,24 @@ import maaj.term.Term;
  */
 public class MapPWrap implements Map {
 
+  private final IPersistentMap<Term, Term> map;
+
+  private MapPWrap(IPersistentMap<Term, Term> map) {
+    this.map = map;
+  }
+
+
   @Override
   public Map retM(Term contents) {
-    throw new UnsupportedOperationException("Not supported yet."); //TODO: implement
+    return MapH.emptyPersistent().conj(contents);
   }
 
   @Override
   public Map bindM(Invocable fn2Monad) {
-    throw new UnsupportedOperationException("Not supported yet."); //TODO: implement
+    MapT m = MapH.emptyTransient();
+    for (KVPair p : this)
+      ((Functor<?>) p.transform(fn2Monad)).foreach((Invocable1) x -> m.doConj(x));
+    return m.asPersistent();
   }
 
   @Override
@@ -37,42 +54,92 @@ public class MapPWrap implements Map {
 
   @Override
   public Int getCount() {
-    throw new UnsupportedOperationException("Not supported yet."); //TODO: implement
+    return Int.of(map.count());
   }
 
   @Override
-  public Map conj(Term t) {
-    throw new UnsupportedOperationException("Not supported yet."); //TODO: implement
+  public int getCountAsInteger() {
+    return (map.count());
   }
 
   @Override
   public Seq seq() {
-    throw new UnsupportedOperationException("Not supported yet."); //TODO: implement
+    return SeqH.iterable2seq(this);
   }
 
   @Override
   public Term valAt(Term key, Term dflt) {
-    throw new UnsupportedOperationException("Not supported yet."); //TODO: implement
+    return map.valAt(key, dflt);
   }
 
   @Override
   public Iterator<KVPair> iterator() {
-    throw new UnsupportedOperationException("Not supported yet."); //TODO: implement
+    return new Iterator<KVPair>() {
+      Iterator<Entry<Term, Term>> it = map.iterator();
+
+      @Override
+      public boolean hasNext() {
+        return it.hasNext();
+      }
+
+      @Override
+      public KVPair next() {
+        return H.buildAssocEntry(it.next());
+      }
+    };
   }
 
   @Override
   public Map assoc(Term key, Term value) {
-    throw new UnsupportedOperationException("Not supported yet."); //TODO: implement
+    return wrap(map.assoc(key, value));
   }
 
   @Override
   public Map dissoc(Term key) {
-    throw new UnsupportedOperationException("Not supported yet."); //TODO: implement
+    return wrap(map.without(key));
   }
 
   @Override
   public MapT asTransient() {
-    throw new UnsupportedOperationException("Not supported yet."); //TODO: implement
+    return MapTWrap.of(map);
+  }
+
+  @Override
+  public String toString() {
+    return print();
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (obj instanceof Term)
+      obj = ((Term) obj).getContent();
+    if (obj instanceof MapPWrap) {
+      return map.equiv(((MapPWrap) obj).map);
+    }
+    //WISH: handle other implementations too
+    return false;
+  }
+
+  @Override
+  public int hashCode() {
+    return map.hashCode();
+  }
+
+  private static MapPWrap wrap(IPersistentMap<Term, Term> pmap) {
+    return new MapPWrap(pmap);
+  }
+  @SuppressWarnings("unchecked")
+  public static MapPWrap of(IPersistentMap<Term, Term> pmap) {
+    //null checked in H.wrap: properly returns Nil on null
+    return wrap(pmap);
+  }
+
+  @SuppressWarnings("unchecked")
+  public static Map ofNil(IPersistentMap<Term, Term> pmap) {
+    //variant that can handle null
+    if (pmap == null)//null-> empty - any other option?
+      return MapH.emptyPersistent();
+    return new MapPWrap(pmap);
   }
 
 }
---
+++ src/main/java/maaj/coll/wrap/MapTWrap.java
@@ -0,0 +1,109 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package maaj.coll.wrap;
+
+import com.github.krukow.clj_lang.IEditableCollection;
+import com.github.krukow.clj_lang.IPersistentMap;
+import com.github.krukow.clj_lang.ITransientMap;
+import java.util.Iterator;
+import maaj.term.Int;
+import maaj.term.Invocable;
+import maaj.term.KVPair;
+import maaj.term.Map;
+import maaj.term.MapT;
+import maaj.term.Term;
+import maaj.util.MapH;
+
+/**
+ *
+ * @author maartyl
+ */
+public class MapTWrap implements MapT {
+
+  private ITransientMap<Term, Term> map;
+
+  private MapTWrap(ITransientMap<Term, Term> map) {
+    this.map = map;
+  }
+
+  @Override
+  public MapT retM(Term contents) {
+    return MapH.emptyTransient().doConj(contents);
+  }
+
+  @Override
+  public MapT bindM(Invocable fn2Monad) {
+    throw new UnsupportedOperationException("Transient maps cennot be iterated over.");
+  }
+
+
+  @Override
+  public Int getCount() {
+    return Int.of(map.count());
+  }
+
+  @Override
+  public int getCountAsInteger() {
+    return (map.count());
+  }
+
+  @Override
+  public Term valAt(Term key, Term dflt) {
+    return map.valAt(key, dflt);
+  }
+
+  @Override
+  public Iterator<KVPair> iterator() {
+    //for now, maybe allow: create persistent, call iterator, replace transient with new .asTransient ....
+    throw new UnsupportedOperationException("Transient maps cennot be iterated over.");
+  }
+
+  @Override
+  public MapT doAssoc(Term key, Term value) {
+    map = map.assoc(key, value);
+    return this;
+  }
+
+  @Override
+  public MapT doDissoc(Term key) {
+    map = map.without(key);
+    return this;
+  }
+
+  @Override
+  public Map asPersistent() {
+    return MapPWrap.of(map.persistentMap());
+  }
+
+  @Override
+  public String toString() {
+    return "#<transient map; size: " + getCountAsInteger() + ">";
+  }
+
+//  @Override
+//  public boolean equals(Object obj) {
+//    return this == obj;
+//  }
+//
+//  @Override
+//  public int hashCode() {
+//    return -1;
+//  }
+
+  @SuppressWarnings("unchecked")
+  static MapTWrap of(IPersistentMap<Term, Term> pmap) {
+    return new MapTWrap((ITransientMap<Term, Term>) ((IEditableCollection<?>) pmap).asTransient());
+  }
+
+  @SuppressWarnings("unchecked")
+  public static MapT ofNil(IPersistentMap<Term, Term> pmap) {
+    //variant that can handle null
+    if (pmap == null)
+      return MapH.emptyTransient();
+    return of(pmap);
+  }
+
+}
