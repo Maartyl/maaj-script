0e49a9bf12c60f5db6680074ea75752e20aaef78
Maartyl <kuronomaartyl@gmail.com>
2015-05-10 01:42:34 +0200

    this will contain defs for 'if', 'def' etc.

---
+++ src/main/java/maaj/lang/CoreLoader.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package maaj.lang;
+
+import maaj.term.Symbol;
+
+/**
+ *
+ * @author maartyl
+ */
+public class CoreLoader extends Namespace.Loader {
+
+  @Override
+  public Namespace loadNamespaceFor(Symbol nsName, Context cxt) {
+    throw new UnsupportedOperationException("Not supported yet."); //TODO: implement
+  }
+
+}
