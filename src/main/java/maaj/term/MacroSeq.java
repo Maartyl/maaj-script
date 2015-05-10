880ac69d9e04170214cf8fc442f46afce83bd94f
Maartyl <kuronomaartyl@gmail.com>
2015-05-10 19:08:58 +0200

    macro seq extends invSeq

--- src/main/java/maaj/term/MacroSeq.java
+++ src/main/java/maaj/term/MacroSeq.java
@@ -5,8 +5,6 @@
  */
 package maaj.term;
 
-import java.io.IOException;
-import java.io.Writer;
 import maaj.lang.Context;
 import maaj.util.H;
 
@@ -15,33 +13,20 @@ import maaj.util.H;
  * <p>
  * @author maartyl
  */
-public final class MacroSeq implements Macro {
-
-  private final Seq fn;
-  private final Context closure;
+public final class MacroSeq extends InvSeq implements Macro {
 
   protected MacroSeq(Seq fn, Context closure) {
-    this.fn = fn;
-    this.closure = closure;
+    super(fn, closure);
   }
 
   @Override
-  public Term invokeSeq(Seq args) {
-    return H.cons(doSym, fn).eval(closure.addToScope(H.map(argsSym, args)));
-  }
-
-  @Override
-  public void show(Writer w) throws IOException {
-    H.cons(macroseqSym, fn).show(w);
+  protected Symbol getShowName() {
+    return macroseqSym;
   }
 
   private static final Symbol macroseqSym = H.symbol("#", "macroseq");
-  private static final Symbol argsSym = H.symbol("$args");
-  private static final Symbol doSym = H.symbol("#", "do");
 
   public static MacroSeq of(Seq fn, Context closure) {
-    return new MacroSeq(fn.fmap((Invocable1) x -> x.evalMacros(closure)), closure);
+    return new MacroSeq(fn, closure);
   }
-
-
 }
