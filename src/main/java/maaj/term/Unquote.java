8a1a3d49d538e448055858ac064597c2fe39bd1a
Maartyl <kuronomaartyl@gmail.com>
2015-05-12 01:07:38 +0200

    unquote

---
+++ src/main/java/maaj/term/Unquote.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package maaj.term;
+
+import java.io.IOException;
+import java.io.Writer;
+import maaj.exceptions.InvalidOperationException;
+import maaj.lang.Context;
+import maaj.util.H;
+import maaj.util.Sym;
+
+/**
+ *
+ * @author maartyl
+ */
+public class Unquote implements Term {
+
+  private final boolean isSplicing;
+  private final Term body;
+
+  public Unquote(boolean isSplicing, Term body) {
+    this.isSplicing = isSplicing;
+    this.body = body;
+  }
+
+  @Override
+  public Monad unquoteTraverse(Context c) {
+    if (isSplicing) 
+      return H.requireMonad(body.eval(c));
+    else 
+      return H.tuple(body.eval(c));
+  }
+
+  @Override
+  public Term eval(Context c) {
+    throw new InvalidOperationException("#macro/unquote outside quotation context");
+  }
+
+  @Override
+  public Term evalMacros(Context c) {
+    throw new InvalidOperationException("#macro/unquote outside quotation context");
+  }
+
+  @Override
+  public Term apply(Context cxt, Seq args) {
+    throw new InvalidOperationException("#macro/unquote outside quotation context");
+  }
+
+
+  @Override
+  public void show(Writer w) throws IOException {
+    H.list(isSplicing ? Sym.unquoteSplicingSymC : Sym.unquoteSymC, body).show(w);
+  }
+}
