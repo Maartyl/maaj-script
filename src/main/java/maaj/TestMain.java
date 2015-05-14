0907226f8f06316c1e6b501bf603ab3537e0e817
Maartyl <kuronomaartyl@gmail.com>
2015-05-14 02:25:42 +0200

    test main : delete old tests

--- src/main/java/maaj/TestMain.java
+++ src/main/java/maaj/TestMain.java
@@ -6,12 +6,6 @@
 package maaj;
 
 import maaj.lang.Repl;
-import maaj.util.H;
-import maaj.util.SeqH;
-import maaj.term.Int;
-import maaj.term.Term;
-import maaj.term.*;
-import maaj.util.Generators;
 
 /**
  *
@@ -19,44 +13,7 @@ import maaj.util.Generators;
  */
 public class TestMain {
   public static void main(String[] args) {
-    Int a = Int.of(3);
-    System.out.println(a.neg());
-
-    Vec v = H.tuple(a);
-
-    SeqH.mapLazy(H.list(Int.of(1), Int.of(2), Int.of(3), Int.of(4)), (Invocable1) x -> {
-      return ((Num) x).inc();
-    }).foreach((Invocable1) x -> {
-      System.out.println(x);
-      return H.NIL;
-    });
-
-    try {
-      Generators.range().foreach((Invocable1) x -> {
-        Int i = H.requireInt(x);
-
-        if (i.gt(Int.of(100)))
-          throw new RuntimeException();
-
-        System.out.print(x);
-        System.out.print(' ');
-        return x;
-      });
-    } catch (RuntimeException e) {
-    }
-    System.out.println("------");
-
-//    System.out.println(H.read1("^^:a :b {}").getMeta());
-//    System.out.println(H.read1("^:a ^:b {}").getMeta());
-
-    System.out.println("------");
-    for (Term t : H.read("1[  2 4  ]3  \"hel\\u1234l\\to\" \n"
-                         + " `(hello? ::from ('this ({}recursive[])test!))  {1 2, 3 4}\n"
-                         + "--5  (a/a/a/a/a/?)")) {
-      System.out.println("--");
-      System.out.println(t.print());
-      //System.out.println(t.evalMacros(new Context()).print());
-    }
+ 
     Repl.main(args);
   }
 
