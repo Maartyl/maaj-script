b39eac201d3033d00e61898e9ee6f9a844ef0423
Maartyl <kuronomaartyl@gmail.com>
2015-05-16 20:01:26 +0200

    term package info

---
+++ src/main/java/maaj/term/package-info.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * This is a package with terms, that can be part of evaluation and accessed from the script and stored in collections.
+ * The collections themselves are also terms.
+ * <p>
+ * Most of these are just interfaces with default methods because they don't need any data as such.
+ * <p>
+ * Invocables make use of being just interfaces, and many of them are functional interfaces.
+ * <p>
+ * //It grew quite large, so I might split it, but I'm not sure how yet.
+ */
+package maaj.term;
