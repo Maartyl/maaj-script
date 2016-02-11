80512fdbdb8a45911745e6b75b0fd0a19a138962
Maartyl <kuronomaartyl@gmail.com>
2016-02-11 12:41:23 +0100

    NsDot: adds deciding about type of macro

--- src/main/java/maaj/lang/NsDot.java
+++ src/main/java/maaj/lang/NsDot.java
@@ -7,6 +7,7 @@ package maaj.lang;
 
 import java.util.Collection;
 import java.util.Collections;
+import maaj.term.Macro;
 import maaj.term.Symbol;
 import maaj.term.Var;
 import maaj.util.Sym;
@@ -52,6 +53,67 @@ public final class NsDot implements Namespace.ReadOnly {
 
   @Override
   public Var getOwn(Symbol name) {
+    return Var.of(matchName(name.getNm()), Sym.macroMapTag);
+  }
+
+  //---
+  private Macro matchName(String name) {
+    switch (name) {
+    case "": throw new UnsupportedOperationException("CANNOT HAPPEN: empty name");
+    case "!!": throw new UnsupportedOperationException("Not supported yet: throw"); //TODO: implement
+    //other special cases here
+    default: return extractName(name);
+    }
+  }
+
+  private Macro extractName(String name) {
+
+    //name is nonempty
+    switch (name.charAt(0)) { //on first char
+    case ':': return ctor(name.substring(1));
+    }
+
+    int lastIndex = name.length() - 1;
+
+    if (lastIndex == 0)
+      throw new UnsupportedOperationException("name has only 1 char"); //TODO: implement
+
+    switch (name.charAt(lastIndex)) { //on last char
+    case ':': return getter(name.substring(0, lastIndex));
+    case '?': return getterBool(name.substring(0, lastIndex));
+    case '!': return setter(name.substring(0, lastIndex));
+    case '-': return field(name.substring(0, lastIndex));
+    }
+
+    //ok, nothing special: just normal method invoke: replace all x-y with xY etc.
+    return escapeAndInvoke(name);
+  }
+
+  private Macro ctor(String substring) {
+    throw new UnsupportedOperationException("Not supported yet."); //TODO: implement
+  }
+
+  private Macro getter(String name) {
+    return tter("get", name);
+  }
+
+  private Macro getterBool(String name) {
+    return tter("is", name);
+  }
+
+  private Macro setter(String name) {
+    return tter("set", name);
+  }
+
+  private Macro tter(String prefix, String name) {
+    return escapeAndInvoke(prefix + name.substring(0, 1).toUpperCase() + name.substring(1));
+  }
+
+  private Macro field(String substring) {
+    throw new UnsupportedOperationException("Not supported yet."); //TODO: implement
+  }
+
+  private Macro escapeAndInvoke(String name) {
     throw new UnsupportedOperationException("Not supported yet."); //TODO: implement
   }
 
