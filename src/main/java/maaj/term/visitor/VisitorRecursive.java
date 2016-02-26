75d049afbf7ee9d424da46fdef052871fb0af783
Maartyl <kuronomaartyl@gmail.com>
2016-02-26 14:31:13 +0100

    nil is ground in visitor

--- src/main/java/maaj/term/visitor/VisitorRecursive.java
+++ src/main/java/maaj/term/visitor/VisitorRecursive.java
@@ -159,7 +159,7 @@ public interface VisitorRecursive<TR, TA> extends Visitor<TR, TA> {
 
   @Override
   default TR nil(Nil t, TA arg) {
-    return id(t, arg);
+    return ground(t, arg);
   }
 
   @Override
