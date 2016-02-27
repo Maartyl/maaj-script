19a1b6889cf1c7c5d6bf6c109db83e5d4bad9972
Maartyl <kuronomaartyl@gmail.com>
2016-02-27 21:30:24 +0100

    adds: doc for IO

--- src/main/java/maaj/term/IO.java
+++ src/main/java/maaj/term/IO.java
@@ -12,7 +12,14 @@ import maaj.term.visitor.Visitor;
 import maaj.util.H;
 
 /**
- *
+ * Represents computation dependent on state of world.
+ * Can mutate more than just current namespace in about any way.
+ * One has to consider time and state when invoking using 'io! .
+ * <p>
+ * Transients and 'def are only other stateful constructs,
+ * but compared to IO they are very limited (to the collection and Vars in current namespace).
+ * There will be a few more stateful constructs, but all very local and safe. (like Atom, ...)
+ * <p>
  * @author maartyl
  */
 @FunctionalInterface
